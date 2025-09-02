c898375a931e1cbc6db677e7cffc2d9a458c3073
dhananjaypai08 <dhananjay2002pai@gmail.com>
2025-09-02 23:17:47 +0530

    fix: rpc chain

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,9 +1,9 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { baseSepolia } from 'viem/chains';
+import { baseSepolia, base } from 'viem/chains';
 
-// Define Base Sepolia with custom RPC
-const baseSepoliaCustom = {
-  ...baseSepolia,
+
+const baseCustom = {
+  ...base,
   rpcUrls: {
     default: { http: [process.env.NEXT_PUBLIC_BASE_SEPOLIA_RPC!] },
     public: { http: [process.env.NEXT_PUBLIC_BASE_SEPOLIA_RPC!] }
@@ -13,8 +13,8 @@ const baseSepoliaCustom = {
 export const config = getDefaultConfig({
   appName: 'Judge.AI Betting',
   projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID!,
-  chains: [baseSepoliaCustom],
+  chains: [baseCustom],
   ssr: true,
 });
 
-export const SUPPORTED_CHAINS = [baseSepoliaCustom];
+export const SUPPORTED_CHAINS = [baseCustom];
